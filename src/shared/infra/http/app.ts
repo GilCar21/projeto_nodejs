8905797a2851166f4de2bed68ccd6d3d470b20f0
Gilberto <gilbertocarvalho1997@gmil.com>
2022-10-13 12:17:31 -0300

    add cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -12,6 +12,8 @@ import "@shared/container";
 import { router } from "./routes";
 import upload from "@config/upload";
 
+import cors from 'cors'
+
 createConnection();
 const app = express();
 
@@ -21,6 +23,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
